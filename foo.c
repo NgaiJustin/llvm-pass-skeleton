125cb4b70989bf25703d7af9fbc8863d52202821
Justin Ngai <jn537@cornell.edu>
2023-09-26 13:57:01 -0400

    Some mutation

--- foo.c
+++
@@ -1,7 +0,0 @@
-int add4(int x) {
-    return x = 4;
-}
-
-int main() {
-    return add4(38);
-}
--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -10,9 +10,32 @@ namespace {
 struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
         for (auto &F : M) {
-            errs() << "I saw a function called " << F.getName() << "!\n";
+            // errs() << "I saw a function called " << F.getName() << "!\n";
+            for (auto &B : F) {
+                // errs() << "\nBasic block.\n";
+                for (auto &I : B) {
+                    // errs() << "Instructions: " << I << "\n";
+
+                    // null if not BinaryOperator
+                    if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+                        IRBuilder<> builder(op);
+
+                        // construct multiply instruction
+                        Value *lhs = op->getOperand(0);
+                        Value *rhs = op->getOperand(1);
+                        Value *mul = builder.CreateMul(lhs,rhs);
+
+                        // replace add instruction
+                        for (auto &U : op->uses()){
+                            User *user = U.getUser();
+                            user->setOperand(U.getOperandNo(), mul);
+                        }
+                    }
+                }
+            }
         }
-        return PreservedAnalyses::all();
+        // return PreservedAnalyses::all();
+        return PreservedAnalyses::none();  // we have mutated the code now
     };
 };
 
