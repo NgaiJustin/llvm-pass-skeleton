d0842f6c4fc40b66a2be7824a0590af93795fce7
Justin Ngai <jn537@cornell.edu>
2023-10-03 18:15:12 -0400

    Update skeleton.cpp for rtlib example

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -2,6 +2,7 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Function.h"
 
 using namespace llvm;
 
@@ -11,26 +12,46 @@ struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
         for (auto &F : M) {
             // errs() << "I saw a function called " << F.getName() << "!\n";
+            
+            LLVMContext& Ctx = F.getContext();
+
+            // F.getParent() is a module
+            auto logFunc = F.getParent()->getOrInsertFunction(
+                // name, return type, arg type
+                "logop", Type::getVoidTy(Ctx), Type::getInt32Ty(Ctx)
+            );
+
             for (auto &B : F) {
                 // errs() << "\nBasic block.\n";
+
                 for (auto &I : B) {
                     // errs() << "Instructions: " << I << "\n";
-
-                    // null if not BinaryOperator
+                    
                     if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+                        
+                        // insert right before function by default
                         IRBuilder<> builder(op);
 
-                        // construct multiply instruction
-                        Value *lhs = op->getOperand(0);
-                        Value *rhs = op->getOperand(1);
-                        Value *mul = builder.CreateMul(lhs,rhs);
+                        // set insertion point right after the op
+                        builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
 
-                        // replace add instruction
-                        for (auto &U : op->uses()){
-                            User *user = U.getUser();
-                            user->setOperand(U.getOperandNo(), mul);
-                        }
+                        Value* args = {op};
+                        builder.CreateCall(logFunc, args);
                     }
+
+                    // // null if not BinaryOperator
+                    // if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+                    //     IRBuilder<> builder(op);
+                    //     // construct multiply instruction
+                    //     Value *lhs = op->getOperand(0);
+                    //     Value *rhs = op->getOperand(1);
+                    //     Value *mul = builder.CreateMul(lhs,rhs);
+                    //     // replace add instruction
+                    //     for (auto &U : op->uses()){
+                    //         User *user = U.getUser();
+                    //         user->setOperand(U.getOperandNo(), mul);
+                    //     }
+                    // }
                 }
             }
         }
